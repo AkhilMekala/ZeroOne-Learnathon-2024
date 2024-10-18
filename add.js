99c68eef21face0bf13d0229eea1d5556f166877
2300030422 <163741127+AkhilMekala@users.noreply.github.com>
2024-10-18 13:18:53 +0530

    2300030422_Akhil Mekala

---
+++ add.js
@@ -0,0 +1,7 @@
+// var
+// let
+let a = "ZeroOne";
+a = 10;
+const n = 10;
+n = "tyson"
+console.log(n);
---
+++ info.js
@@ -0,0 +1,7 @@
+let name = "AKHIL MEKALA";
+let rollno = 2300030422;
+let branch = "CSE";
+
+console.log(name);
+console.log(rollno);
+console.log(branch);
